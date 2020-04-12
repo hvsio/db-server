be7bb6b2f61cfbb76b6aa5f450a147eeeed87533
hvsio <h4sio@o2.pl>
2020-04-12 10:18:20 +0200

    authorization, idk if I will ever use it

---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt')
+
+const userSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+})
+
+userSchema.pre('save', function (next) {
+    if (!this.isModified('password')) {
+        return next()
+    }
+
+    bcrypt.hash(this.password, 8, (err, hash) => {
+        if (err) {
+            return next(err)
+        }
+
+        this.password = hash
+        next()
+    })
+})
+
+userSchema.methods.checkPassword = function (password) {
+    const passwordHash = this.password
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, passwordHash, (err, same) => {
+            if (err) {
+                return reject(err)
+            }
+
+            resolve(same)
+        })
+    })
+}
+
+module.exports = mongoose.model('user', userSchema);
--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const config = require('./environment/env.config'),
     bodyParser = require('body-parser'),
     monk = require('monk');
 
-// app.use(function(req, res) {
-//     res.status(404).send({url: req.originalUrl + ' not found'})
-// });
-
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/events', { useNewUrlParser: true }).
@@ -27,66 +23,3 @@ routes(app); //register the route
 
 app.listen(port);
 console.log("Listening on port: " + port);
-
-
-// async function getArray() {
-//         dbo.listCollections().toArray().then((docs) => {
-//             console.log(docs);
-//         }).catch((err) => {
-//             console.log(err);
-//         });
-// }
-//
-// async function checkStatistics() {
-//         dbo.stats((err, stats) => {
-//             if (err) throw err;
-//             console.log(stats);
-//         })
-// }
-//
-// async function addEvent(name, start, end, category, day) {
-//     var obj = {
-//         name: name,
-//         start: start,
-//         end: end,
-//         category: category,
-//         day: day
-//     };
-//         dbo.collection("events").insertOne(obj, function (err, res) {
-//             if (err) throw err;
-//             console.log('First obj inserted');
-//
-//         });
-// }
-//
-// async function getEventsFromDay(day) {
-//         query = {day: day};
-//         dbo.collection("events").find(query).toArray(function (err, result) {
-//             if (err) throw err;
-//             console.log(result);
-//         })
-// }
-//
-// async function getEventsFromCategory(category) {
-//         query = {category: category};
-//         dbo.collection("events").find(query).toArray(function (err, result) {
-//             if (err) throw err;
-//             console.log(result);
-//         })
-// }
-//
-// async function deleteEvents(event_id) {
-//         query = {_id: event_id};
-//         dbo.collection("events")._deleteOne(query, function (err, obj) {
-//             if (err) throw err;
-//             console.log("1 document deleted: " + event_id);
-//         })
-// }
-//
-// async function deleteMany(day) {
-//         query = {day: day};
-//         dbo.collection("events").deleteMany(query, function (err, obj) {
-//             if (err) throw err;
-//             console.log(obj + " entries deleted");
-//         })
-// }
